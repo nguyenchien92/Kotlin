7a97a7f9bdb0d90c50c6c9175d85b2c57ed99363
Nguyen Chien <chiennxd00245@fpt.edu.vn>
2019-12-09 09:35:37 +0700

    Fake app Tin Do Hang Hieu

---
+++ app/src/main/java/com/example/nguyen/fakeapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nguyen.fakeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
